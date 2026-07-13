59dc59c108218ac3c17d53aefa6291eabfe588d5
magarsanchita1110697-arch <magarsanchita1110697@gmail.com>
2026-07-13 12:41:51 +0530

    Add 1783-A solution

---
+++ A_Make_it_Beautiful.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--){
+         int n;
+         cin>>n;
+
+         vector<int>a(n);
+        for(int i=0; i<n ; i++){
+            cin>>a[i];
+        }
+        sort(a.begin(),a.end());
+        
+
+        if(a[0]== a[n-1]){
+            cout<<"NO"<<"\n";
+        }
+
+        else{
+            cout<<"YES"<<'\n';
+            cout<<a.back()<<" ";
+
+         for(int i = 0; i<n-1; i++){
+            cout<<a[i]<<" ";
+        }
+        cout<<'\n';
+        }
+    }
+}

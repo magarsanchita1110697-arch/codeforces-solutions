e23991a7d19f189215cf0ea2224845ddb5d1a35a
magarsanchita1110697-arch <magarsanchita1110697@gmail.com>
2026-07-17 23:29:29 +0530

    Add 1814A Solution

---
+++ A_Coins.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+   int t;
+   cin>>t;
+   while (t--){
+     
+    long long n,k;
+    cin>>n>>k;
+
+    if(n % 2 == 0 || k%2==1){
+        cout<<"YES"<<'\n';
+    }
+
+     else{
+        cout<<"NO"<<'\n';
+     }
+
+    }
+
+}
